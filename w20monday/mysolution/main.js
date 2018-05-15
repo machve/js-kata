4cf93030052f0a8215686a7548325f62a0b5cf8c
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-15 19:22:08 +0200

    button on clicl closes

--- w20monday/mysolution/main.js
+++ w20monday/mysolution/main.js
@@ -1,9 +1,17 @@
 "use strict";
 // on window load move div up
+const div = document.querySelector("div");
 window.addEventListener("load",function movediv (){
 console.log("hi mom")
-const div = document.querySelector("div");
+
+
 div.style.transform = "translateY(-100px)";
 
 })
 
+// button on click close div
+const button = document.querySelector("button");
+button.addEventListener('click',function(){
+
+    div.style.transform = "translateY(100px)";
+})
