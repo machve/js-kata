42f218703dc5e46f3e92d70038b091f9fd5382de
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-16 22:21:52 +0200

    making mockify function

--- w20tuesday/mysolution/mockify.js
+++ w20tuesday/mysolution/mockify.js
@@ -1,12 +1,28 @@
 "use strict";
+//     let text = document.querySelector('h1').textContent;
+// const letter = Array.from(text);
+// console.log(letter)
+// letter.forEach(mockify);
+
+// function mockify (l){
+// document.querySelector("#mocking").innerHTML =letter.map(l=>Math.round(Math.random()) ? l.toUpperCase() : l.toLowerCase()).join("");
+// }
+
+//  mockify();
 
 
-let text = document.querySelector('h1').textContent;
-const letter = Array.from(text);
-console.log(letter)
-let lettermock = letter.map(l=>Math.round(Math.random()) ? l.toUpperCase() : l.toLowerCase());
-console.log(lettermock)
-lettermock.toString();
 
-document.querySelector("#mocking").innerHTML = lettermock.join("")
+function mockify (text) {
+if (typeof text === 'string'){
+    console.log("yep")
+    let arrayText = Array.from(text);
+    document.querySelector("#mocking").innerHTML =arrayText.map(l=>Math.round(Math.random()) ? l.toUpperCase() : l.toLowerCase()).join("");
+}
+else {
+    console.log('nope')
 
+}
+}
+mockify();
+// document.querySelector("#mocking").innerHTML =letter.map(l=>Math.round(Math.random()) ? l.toUpperCase() : l.toLowerCase()).join("");
+// }
