c6c4c9d414250d0add598c77fc15e8ca11feaac3
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-28 20:13:49 +0200

    displaying english tweets and user name

--- w22monday/mysolution/main.js
+++ w22monday/mysolution/main.js
@@ -1,23 +1,42 @@
 "use strict"
 
+let template = document.querySelector(".link-template").content;
+let section = document.querySelector('section');
+
 function getTweets() {
-   
+
 
     fetch("http://kea-alt-del.dk/twitter/api/?count=50/?hashtag=word").then(res => res.json())
-    .then(showData);
+        .then(showData);
 }
 
-function showData(json){
+function showData(json) {
     let tweet = json.statuses;
-    console.log(tweet);
-tweet.forEach(status => {
-    if (status.lang === "en"){
-    console.log(status.text)}
-})
-    // json.forEach(function(tweet){
-    
-    // console.log("hi")
-    // })
-};
 
-    getTweets();
+    console.log(tweet);
+
+
+    tweet.forEach(status => {
+
+        let text = status.text;
+        let user = status.user.name;
+        let language = status.lang;
+        console.log(user);
+
+
+        let clone = template.cloneNode(true);
+
+
+
+     if (status.lang === "en"){
+        clone.querySelector(".tweet-text").textContent= text;
+        clone.querySelector(".user-name").textContent =user;
+      }
+        //     // json.forEach(function(tweet){
+
+        //     // console.log("hi")
+        //     // })
+        section.appendChild(clone);
+    });
+}
+getTweets()
