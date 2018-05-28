90ce96214b61b3a1829574657eae1cf0fefa3cbe
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-28 18:31:53 +0200

    fizzbuzz done

---
+++ w21friday/mysolution/fizzbuzz.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 > 0) {
+        console.log("fizz");
+    }
+
+    if (i % 5 === 0 && i % 3 > 0) {
+        console.log("buzz")
+    }
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz")
+    }
+
+    if (i % 3 > 0 && i % 5 > 0) {
+        console.log(i)
+    }
+
+};
